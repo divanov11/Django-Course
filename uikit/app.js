2deba6f00b74d0a9f308deebe96119a547fd4882
Dennis Ivy <33843378+divanov11@users.noreply.github.com>
2021-06-19 13:00:30 -0400

    Update app.js

--- uikit/app.js
+++ uikit/app.js
@@ -2,3 +2,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
+
+
+
+let alertWrapper = document.querySelector('.alerts--wrapper')
+let alertClose = document.querySelector('.alert__close')
+
+
+if (alertWrapper) {
+  alertClose.addEventListener('click', () =>
+    alertWrapper.style.display = 'none'
+  )
+}
