212abbe9f375c30399b6a9213509386200ec8513
Dennis Ivy <33843378+divanov11@users.noreply.github.com>
2021-06-29 09:03:20 -0400

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -127,8 +127,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dennis.from.email@gmail.com'
-EMAIL_HOST_PASSWORD = 'kdojhpzitvcigozh'
+EMAIL_HOST_USER = 'youremail'
+EMAIL_HOST_PASSWORD = 'youremailpassword'
 
 
 # Static files (CSS, JavaScript, Images)
